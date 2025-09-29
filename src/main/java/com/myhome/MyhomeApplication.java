e9c16a770a4daed042345a7e3023834d58f6286f
ggjjrrw <1025608059@qq.com>
2025-09-29 15:04:16 +0800

    工程初始化：添加 SpringBoot 主入口

---
+++ src/main/java/com/myhome/MyhomeApplication.java
@@ -0,0 +1,11 @@
+package com.myhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyhomeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyhomeApplication.class, args);
+    }
+}
